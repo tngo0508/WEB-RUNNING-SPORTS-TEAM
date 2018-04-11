5395942d62421d12ef9f776157c8d9e96781dc44
danhpham <danhpham312@gmail.com>
2018-04-11 02:00:31 -0700

    Delete config.php

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'manager');
-define('DB_PASSWORD', '123456');
-define('DB_NAME', 'SPORTS_MANAGEMENT');
-
-
-?>
