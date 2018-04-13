f3d5530b75d904beacd894276bb5ed615f9a889b
Thomas Ngo <tngo0508@gmail.com>
2018-04-13 01:19:37 -0700

    fixing front-end welcome.php

--- NEW TEST(USE THIS ONE)/footer.php
+++ NEW TEST(USE THIS ONE)/footer.php
@@ -1,7 +1,6 @@
   <!-- page footer -->
   <footer>
+    <link rel="stylesheet" href="css/footer.css">
       <p>&copy; CPSC431 SPORTS TEAM WEBSITE DESINGING.<br />
   </footer>
-
-</body>
-</html>
+  
--- NEW TEST(USE THIS ONE)/header.php
+++ NEW TEST(USE THIS ONE)/header.php
@@ -8,50 +8,15 @@
 	<meta name="keywords" content="web design">
 	<meta name="author" content="TEAM CPSC431">
 	<title>BASKETBALL TEAM MANAGEMENT | Welcome</title>
-	<!-- <link rel="stylesheet" href="./css/welcome.css"> -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+
 </head>
 
-<body>
 	<header>
 		<div id="top">
 			<img src="./logo/1.jpg" alt="CSUF LOGO" height="100" width="100" />
 			<h1> BASKETBALL TEAM MANAGEMENT </h1>
 		</div>
-		<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-			<a class="navbar-brand" href="#">TEAM MANAGEMENT</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-			<div class="collapse navbar-collapse" id="navbarNavDropdown">
-				<ul class="navbar-nav">
-					<li class="nav-item active">
-						<a class="nav-link" href="welcome.php">Welcome <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="rule.html">Rule</a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="contact.html">Contact</a>
-					</li>
-					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
-          Dropdown link
-        </a>
-						<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</li>
-				</ul>
-			</div>
-		</nav>
 	</header>
 
-	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-</body>
 
 </html>
