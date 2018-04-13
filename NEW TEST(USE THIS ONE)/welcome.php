90e084054dd04c7642fe6d8e3d0e398627025043
Thomas Ngo <tngo0508@gmail.com>
2018-04-12 22:55:38 -0700

    Merge branch 'master' of https://github.com/Dpham181/WEB-RUNNING-SPORTS-TEAM

--- NEW TEST(USE THIS ONE)/welcome.php
+++ NEW TEST(USE THIS ONE)/welcome.php
@@ -1,61 +1,3 @@
-<<<<<<< HEAD
-<?php
-     require "header.php";
-
-?>
-  <!DOCTYPE html>
-  <html>
-
-  <head>
-    <meta charset="utf-8">
-    <title>Welcome Page</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-    <link rel="stylesheet" href="./css/welcome.css">
-  </head>
-
-  <body>
-    <div class="container">
-      <div>
-        <section id="logining">
-          <h1>Login form</h1>
-          <?php require_once('login.php') ?>
-
-          <form action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]); ?>" method="post">
-            <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-              <label>Email:<sup>*</sup></label>
-              <input type="text" name="email" class="form-control">
-              <span class="help-block"><?php echo $email_err; ?></span>
-            </div>
-            <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-              <label>Password:<sup>*</sup></label>
-              <input type="password" name="password" class="form-control">
-              <span class="help-block"><?php echo $password_err; ?></span>
-            </div>
-
-            <div class="form-group">
-              <input type="submit" class="btn btn-primary" value="Submit">
-            </div>
-            <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-          </form>
-        </section>
-      </div>
-      <div class="d-flex align-item-end"class="logo"><img id="logo2" src="./logo/2.jpg"></div>
-    </div>
-
-
-
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
-  </body>
-
-  </html>
-
-  <?php
-  require "footer.php";
-  ?>
-=======
-
 <?php
      require "header.php";
 
@@ -97,4 +39,3 @@
   <?php
   require "footer.php";
   ?>
->>>>>>> 6bef9a101311aa59de381b0d5623d247524ced76
