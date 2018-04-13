f137c55122173d4b4a8938ff42b5a0909b769076
Thomas Ngo <tngo0508@gmail.com>
2018-04-13 10:57:15 -0700

    finish styling welcome.php

--- NEW TEST(USE THIS ONE)/welcome.php
+++ NEW TEST(USE THIS ONE)/welcome.php
@@ -41,40 +41,51 @@
         </ul>
       </div>
     </nav>
-  <div class="container">
-    <?php require_once('login.php') ?>
-    <form class="form-login" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]); ?>" method="post">
-      <h2 class="form-login-header">Account Login</h2>
-      <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-        <label>Email:<sup>*</sup></label>
-        <input type="text" name="email" class="form-control" placeholder="example@email.com" autofocus required>
-        <span class="help-block"><?php echo $email_err; ?></span>
-      </div>
-      <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-        <label>Password:<sup>*</sup></label>
-        <input type="password" name="password" class="form-control" placeholder="password" required>
-        <span class="help-block"><?php echo $password_err; ?></span>
+    <section>
+
+      <div class="container">
+        <?php require_once('login.php') ?>
+        <div class="row align-items-center">
+          <div class="col">
+            <form class="form-login" action="<?php echo htmlspecialchars($_SERVER[" PHP_SELF "]); ?>" method="post">
+              <h2 class="form-login-header">Account Login</h2>
+              <div class="form-group <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
+                <label>Email:<sup>*</sup></label>
+
+                <input type="text" name="email" class="form-control" placeholder="example@email.com" autofocus required>
+                <span class="help-block"><?php echo $email_err; ?></span>
+              </div>
+              <div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                <label>Password:<sup>*</sup></label>
+                <input type="password" name="password" class="form-control" placeholder="password" required>
+                <span class="help-block"><?php echo $password_err; ?></span>
+              </div>
+
+              <div class="form-group">
+                <button id="submit-button" type="submit" class="btn btn-primary" value="Submit">Submit</button>
+                <button type="reset" class="btn btn-primary" name="button">Reset</button>
+              </div>
+              <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
+              <a href="forgot_pass.php">Forgot password?</a>
+            </form>
+          </div>
+        </div>
+
       </div>
 
-      <div class="form-group">
-        <button id="submit-button" type="submit" class="btn btn-primary" value="Submit">Submit</button>
-        <button type="reset" class="btn btn-primary" name="button">Reset</button>
-      </div>
-      <p>Don't have an account? <a href="register.php">Sign up now</a>.</p>
-      <a href="forgot_pass.php">Forgot password?</a>
-    </form>
     <!-- <div>
-      <img src="./logo/2.jpg">
-    </div> -->
-  </div>
+    <img src="logo/2.jpg">
+  </div> -->
+    </section>
 
 
-  <?php
+
+    <?php
     require 'footer.php';
   ?>
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+      <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </body>
 
 
